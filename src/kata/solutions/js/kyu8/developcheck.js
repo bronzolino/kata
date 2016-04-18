990e3d26fcd1f7501c7a1cba66471f893f66d5ff
gabriele <gabriele.locusta>
2016-04-19 01:16:34 +0200

    Preparato l'ambiente per js. Da Verificaregit add .

---
+++ src/kata/solutions/js/kyu8/developcheck.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function develop() {
+    var circleArea = function(radius) {
+        return "area di " + radius;
+    };
+    
+    alert(circleArea(1));
+}
